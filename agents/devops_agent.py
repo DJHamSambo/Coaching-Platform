7462f1bd51be0cae2fd8b90ac68c129d1fd63edf
Hamish Armstrong <hamish.armstrong88@gmail.com>
2026-09-09 06:30:51 +1000

    Move Static Web App to westus2: eastasia preflight rejects new sites

--- agents/devops_agent.py
+++ agents/devops_agent.py
@@ -69,7 +69,7 @@ ENVIRONMENTS = ("nonprod", "prod")
 _DEFAULT_APP_NAME = "coaching-platform"
 # Azure Static Web Apps are only deployable to a handful of regions
 # (centralus, eastus2, westus2, westeurope, eastasia), so main.bicep gives the
-# SWA its own staticWebAppLocation parameter (default eastasia) while
+# SWA its own staticWebAppLocation parameter (default westus2) while
 # everything else uses the primary region below.
 _DEFAULT_REGION = "australiaeast"
 
@@ -596,8 +596,8 @@ param environmentName string
 @description('Azure region for all resources')
 param location string = 'australiaeast'
 
-@description('Region for the Static Web App -- Microsoft.Web/staticSites is only available in centralus, eastus2, westus2, westeurope and eastasia')
-param staticWebAppLocation string = 'eastasia'
+@description('Region for the Static Web App -- Microsoft.Web/staticSites is only available in centralus, eastus2, westus2, westeurope and eastasia; eastasia preflight rejects new sites on this subscription, so westus2 is used')
+param staticWebAppLocation string = 'westus2'
 
 @description('Region for the auto-shutdown Automation account -- Free Trial/Student subscriptions can only create them in eastus, eastus2, westus, northeurope, southeastasia and japanwest')
 param automationLocation string = 'southeastasia'
